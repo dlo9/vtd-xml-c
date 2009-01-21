fdb03f3b5bab1e414b009e236eae63751e6a5918
jzhang2004 <>
2009-01-21 03:55:37 +0000

    release year number change

--- ximple-dev_c/vtd-xml/lex.yy.c
+++ ximple-dev_c/vtd-xml/lex.yy.c
@@ -8022,7 +8022,7 @@ YY_CHAR *yytext;
 #define INITIAL 0
 #line 2 "l8.l"
 /* 
-* Copyright (C) 2002-2008 XimpleWare, info@ximpleware.com
+* Copyright (C) 2002-2009 XimpleWare, info@ximpleware.com
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
